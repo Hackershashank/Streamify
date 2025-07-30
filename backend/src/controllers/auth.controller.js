60a496638b8f69d83c30ea8dcb2ed69898b839e0
Shashank <shashanksingh14370@gmail.com>
2025-07-30 23:30:06 +0530

    setting up and optimizing the codebase

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req,res){
+    res.sent("Signup route");
+}
+export async function login(req,res){
+    res.sent("Login route");
+}
+export function logout(req,res){
+    res.sent("Logout route");
+}
